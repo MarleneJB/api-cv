d8644d40f0f021b35b41e4018294c4844902804d
MarleneJB <marlen10224@gmail.com>
2023-09-09 20:36:02 -0600

    complete 2 header and work-experience

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.options('*', cors());
 const port = 8080;
 
 app.get('/', (req, res, next) => {
-	res.send('node express api <br> by adsoft');
+	res.send('node express api <br> by Marlene');
 });
 
 
 app.get('/header', (req, res, next) => {
   res.sendfile("assets/header.json");
+});
+
+app.get('work-experience', (req, res, next) => {
+	  res.sendfile("assets/work-experience.json");
 });
 
 app.listen(port,  () => 
